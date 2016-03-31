858f9a5c7e36588c41c68343d474257f1f6fd561
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-31 16:09:57 +0200

    angular module

--- app/components/components.ts
+++ app/components/components.ts
@@ -6,6 +6,7 @@ import Main      from './main/main';
 import Dashboard from './dashboard/dashboard';
 import Message   from './message/message';
 import Profile   from './profile/profile';
+import Rest      from './rest/rest';
 import Showcase  from './showcase/showcase';
 import Table     from './table/table';
 
@@ -16,6 +17,7 @@ let components = angular.module('app.components', [
   Dashboard,
   Message,
   Profile,
+  Rest,
   Showcase,
   Table,
 
