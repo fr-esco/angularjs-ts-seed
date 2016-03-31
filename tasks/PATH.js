b6b352b48e48945ac6bbf056589ecafebe6fc1a4
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-31 16:16:55 +0200

    dependencies (lodash)

--- tasks/PATH.js
+++ tasks/PATH.js
@@ -61,6 +61,7 @@ module.exports = {
         './node_modules/ng-showdown/dist/ng-showdown.min.js',
         './node_modules/ng-showdown/dist/ng-showdown.min.js.map',
 
+        './node_modules/restangular/node_modules/lodash/lodash.min.js',
         './node_modules/restangular/dist/restangular.js',
 
         './node_modules/angular-typescript/lib/at-angular.js',
