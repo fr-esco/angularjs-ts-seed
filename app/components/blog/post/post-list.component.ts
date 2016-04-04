ca6aaefe35406b8e2c2142fac894054f4ad4a0a8
Francesco Colamonici <f.colamonici@gmail.com>
2016-04-04 15:11:45 +0200

    post-list

--- app/components/blog/post/post-list.component.ts
+++ app/components/blog/post/post-list.component.ts
@@ -15,17 +15,26 @@ export default class PostListComponent implements at.OnInit, at.OnActivate {
   public title: string;
   public posts: IPost[];
 
+  public searchText: string;
+
   constructor(private postClient: PostClient,
     private log: angular.ILogService) {
     log.debug(['ngComponent', ngComponentName, 'loaded'].join(' '));
   }
 
   public $onInit() {
-    this.postClient.search()
-      .then(data => this.posts = data);
+
   }
 
   public $routerOnActivate(next: at.ComponentInstruction) {
     this.title = next.routeData.data['title'];
+    return this.postClient.search()
+      .then(data => this.posts = data);
+  }
+
+  public search() {
+    let filter = { title_like: this.searchText };
+    return this.postClient.search(filter)
+      .then(data => this.posts = data);
   }
 }
