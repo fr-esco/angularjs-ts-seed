97ae0ebcc43ab7d7699e23ffdfc8b4bc49ebf8af
Francesco Colamonici <f.colamonici@gmail.com>
2016-04-08 11:02:07 +0200

    showcase added to blog demo

--- app/components/blog/blog.component.ts
+++ app/components/blog/blog.component.ts
@@ -17,6 +17,37 @@ const ngComponentName = 'tsfnBlog';
 export default class BlogComponent implements angular.OnActivate {
   public title: string;
 
+  private files = [
+    [
+      'components/blog/blog.scss',
+      'components/blog/blog.module.ts',
+      'components/blog/blog.component.html',
+      'components/blog/blog.component.ts',
+    ], [
+      'components/blog/post/post.scss',
+      'components/blog/post/post.module.ts',
+      'components/blog/post/post.model.ts',
+      'components/blog/post/post-list.component.html',
+      'components/blog/post/post-list.component.ts',
+      'components/blog/post/post-detail.component.html',
+      'components/blog/post/post-detail.component.ts',
+      'components/blog/post/post-comments.component.html',
+      'components/blog/post/post-comments.component.ts',
+      'components/blog/post/post-create.component.html',
+      'components/blog/post/post-create.component.ts',
+      'components/blog/post/post-update.component.html',
+      'components/blog/post/post-update.component.ts',
+      'components/blog/post/post-form.component.html',
+      'components/blog/post/post-form.component.ts',
+    ], [
+      'components/blog/post/post-client.service.ts',
+      'components/blog/comment/comment-client.service.ts',
+      'components/rest/rest.config.ts',
+      'components/rest/rest.model.ts',
+      'components/rest/rest.module.ts',
+    ]
+  ];
+
   constructor(private log: angular.ILogService) {
     log.debug(['ngComponent', ngComponentName, 'loaded'].join(' '));
   }
