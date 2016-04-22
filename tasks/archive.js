9ee9689f2ea703e9cf1f2afa65c9e42c1bbcd955
Francesco Colamonici <f.colamonici@gmail.com>
2016-04-22 12:00:49 +0200

    gulp task setup

--- tasks/archive.js
+++ tasks/archive.js
@@ -4,10 +4,12 @@ var PATH = require('./PATH');
 
 var fs = require('fs');
 var gulp = require('gulp');
+var electron = require('gulp-electron-packager');
 var zip = require('gulp-zip');
 var pkg = require('../package.json');
 var gutil = require('gulp-util');
 var path = require('path');
+var yargs = require('yargs');
 
 gulp.task('build.prod.zip', ['build.prod'], function() {
 
@@ -17,5 +19,37 @@ gulp.task('build.prod.zip', ['build.prod'], function() {
     return gulp.src(PATH.dest.prod.all + '/**/*.*')
         .pipe(zip(filename))
         .pipe(gulp.dest(PATH.dest.pkg.prod));
+
+});
+
+gulp.task('build.prod.win64'/*, ['build.prod']*/, function() {
+    var argv = require('yargs').reset()
+        .usage('Usage: gulp build.prod.win32 -n')
+        .alias('n', 'name')
+        .string('n')
+        .default('n', 'showcase')
+        .describe('n', 'Application Name')
+
+        .alias('s', 'support')
+        .help('s')
+        .argv;
+
+    var options = {
+        name: argv.name,
+        appname: argv.name,
+        appVersion: pkg.version,
+
+        dir: PATH.dest.prod.all,
+        out: PATH.dest.pkg.prod,
+        platform: 'win32',
+        arch: 'x64',
+        prune: true,
+        overwrite: true,
+        version: pkg.devDependencies['electron-prebuilt'].substr(1),
+        ignore: '(ts|packages)'
+    };
+
+    return gulp.src('')
+        .pipe(electron(options, console.error.bind(console)));
 
 });
