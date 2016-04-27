a30d3994563da4d2f8806f57811167e42f8b9e87
Francesco Colamonici <f.colamonici@gmail.com>
2016-04-27 17:40:07 +0200

    env cfg schema verification

--- generator/constant.ts
+++ generator/constant.ts
@@ -1,5 +1,7 @@
+'use strict';
+
 export default angular.module('<%- moduleName %>'<% if (deps) { %>, <%= JSON.stringify(deps) %><% } %>)
-<% constants.forEach(function(constant) { %>
+<% constants.forEach(function (constant) { %>
 .constant('<%- constant.name %>', <%= constant.value %>)
 <% }) %>
 .name;
