e764675e9a18f2ef2b59443de096df6109384500
Luca Oliano <lucaoliano@hotmail.it>
2016-05-12 15:35:07 +0200

    fix translation issue due to valdr example

--- app/components/profilevaldrexample/profilevaldr.config.ts
+++ app/components/profilevaldrexample/profilevaldr.config.ts
@@ -6,20 +6,11 @@
 'use strict';
 
 class ProfileModuleConfiguration {
-  @at.injectMethod('valdrProvider', 'valdrMessageProvider', '$translateProvider')
-  public static valdrConf(valdrProv, valdrMsgProv, translateProv) {
+  @at.injectMethod('valdrProvider', 'valdrMessageProvider')
+  public static valdrConf(valdrProv, valdrMsgProv) {
     valdrProv.setConstraintUrl('./components/profilevaldrexample/constraints.json');
 
     valdrProv.addValidator('customValidator');
-    translateProv.translations('en', {
-      'message.size': '{{fieldName}} must be between {{min}} and {{max}} characters.',
-      'Person.firstName': 'firstName'
-    });
-
-    translateProv.translations('it', {
-      'message.size': '{{fieldName}} deve essere compreso tra {{min}} e {{max}}.',
-      'Person.firstName': 'Nome'
-    });
  };
 }
 export default ProfileModuleConfiguration.valdrConf;
