39a59c66c91cc4224cdd43516b25183701e6b8da
Francesco Colamonici <f.colamonici@gmail.com>
2016-08-22 15:12:28 +0200

    launchers

--- tasks/karma.conf.js
+++ tasks/karma.conf.js
@@ -138,7 +138,7 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-    //browsers: ['PhantomJS'],
+    // browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
